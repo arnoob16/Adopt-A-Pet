5272a22c5d1bb968aa5ca2a9894baaf44a5d47b1
Arnab <contact.arnab16@gmail.com>
2021-01-02 21:57:39 +0530

    Added Pet & Results Component with async API Calls

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
+export default function Pet({ name, animal, breed, media, location, id }) {
+    
+    let placeHolderImage = 'http://placeorgi.com/300/300';
+    if(media.length){
+        placeHolderImage = media[0].small;
+    }
+
+    return (
+        <a href={`/details/${id}`} className = "pet">
+            <div className="image-container">
+                <img src={placeHolderImage} alt={name}/>
+            </div>
+            <div className="info">
+                <h1>{name}</h1>
+                <h2>{`${animal} - ${breed} - ${location}`}</h2>
+            </div>
+        </a>
+    )
 };
